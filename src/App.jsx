b9613409621a02c64b74dc52b46f5d6df9a9b6cb
donalynando <donalynando251@gmail.com>
2025-02-12 10:52:53 +0800

    this is new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-return <h1>
-  HELLO WORLD
-</h1>
+  return <h1>
+    HELLO Earth!
+  </h1>
 }
 
 export default App
